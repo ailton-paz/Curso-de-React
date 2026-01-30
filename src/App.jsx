0109ae1e7942a2ca16cec53728cf540c20c040f9
Seu Nome Completo <seuemail@github.com>
2026-01-30 17:26:44 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ const [tasks, setTasks] = useState([{
   isCompleted: false
 }])
 
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px]">
